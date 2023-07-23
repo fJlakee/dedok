2b54cfabafc978af9205cb31480374507e72853c
fjlakee <bissenovadilet@gmail.com>
2023-07-23 22:58:07 +0500

    finished tasks between isLessOrEqual() and substring()

--- abc-js/practice/utils/number-utils/is-float.js
+++ abc-js/practice/utils/number-utils/is-float.js
@@ -1,2 +1,9 @@
 export function isFloat(value) {
+	if(typeof value !== "number"){
+		throw new Error("value must be only number type");
+	}
+	if(value%1 === 0){
+		return false
+	}
+	return true;
 }
--- abc-js/practice/utils/string-utils/reverse.js
+++ abc-js/practice/utils/string-utils/reverse.js
@@ -1,3 +1,8 @@
 /** Возвращает "перевернутую" копию text */
 export function reverse(text) {
+	if(typeof text !== "string"){
+		throw Error("argument must be type of string");
+	}
+	let newtext = text.split("").reverse().join('');
+	return newtext;
 }
