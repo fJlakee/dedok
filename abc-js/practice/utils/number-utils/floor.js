d06b3a77c05565e996f857bb1e6212869bf55104
fjlakee <bissenovadilet@gmail.com>
2023-07-27 15:20:15 +0500

    finished number utils

--- abc-js/practice/utils/number-utils/floor.js
+++ abc-js/practice/utils/number-utils/floor.js
@@ -1,3 +1,24 @@
 /** возвращает целую часть числа уменьшенную в меньшую сторону */
 export function floor(value) {
+    if(typeof value !== "number"){
+        throw Error("value must be only number type");
+    }
+    if(value%1 === 0){
+        return value;
+    }
+    if(value%1 > 0.5){
+        return (value - value%1);
+    }
+    else{
+        if(value >= 1){
+            return (value - value%1);
+        }
+        else if (value >= 0 && value < 1){
+            return 0;
+        }
+        else{
+            return (value - value%1) - 1;
+        }
+    }
+
 }
